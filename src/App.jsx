9b06e81b85979021c4b8d47fc93806827c63d2ba
nodirchik6903 <nu2913634@gmail.com>
2024-04-05 18:04:40 +0500

    login page end

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,19 @@ import { Route, Routes } from "react-router-dom";
 import Login from "./components/Login/Login";
 import AddCategory from "./components/AddCategory/AddCategory";
 import Main from "./components/Main/Main";
-import EditCategory from './components/EditCategory/EditCategory';
-import "react-toastify/dist/ReactToastify.css"
+import EditCategory from "./components/EditCategory/EditCategory";
+import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 
 function App() {
-  
-
   return (
     <>
-    <ToastContainer />
+      <ToastContainer />
       <Routes>
-      <Route path="/" element={<Main />} />
-      <Route path='/login' exact  element={<Login />} />
-      <Route path="/addcategory" element={<AddCategory />} />
-      <Route path="/edit/:id" element={<EditCategory />} />
-      {/* <Route path="/addcategory" element={<AddCategory />} /> */}
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/addcategory" element={<AddCategory />} />
+        <Route path="/edit/:id" element={<EditCategory />} />
       </Routes>
     </>
   );
