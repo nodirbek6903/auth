29d8a8d1f5292e69eb71bebab11a13bdeae6c35b
nodirchik6903 <nu2913634@gmail.com>
2024-04-11 23:20:08 +0500

    errors delete

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,20 @@ import Main from "./components/Main/Main";
 import EditCategory from "./components/EditCategory/EditCategory";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
+import { useEffect, useState } from "react";
+import {useNavigate} from "react-router-dom"
 
 function App() {
+  const [token,setToken] = useState(null)
+  const navigate = useNavigate()
+  
+  useEffect(() => {
+    const storedToken = localStorage.getItem("access_token");
+    setToken(storedToken)
+    if(!storedToken){
+      navigate("/login")
+    }
+  },[token])
   return (
     <>
       <ToastContainer />
--- src/components/EditCategory/EditCategory.jsx
+++ src/components/EditCategory/EditCategory.jsx
@@ -7,42 +7,37 @@ const EditCategory = () => {
   const [nameEN, setNameEN] = useState("");
   const [nameRU, setNameRU] = useState("");
   const [images, setImage] = useState([]);
+  const [prevImage,setPrevImage] = useState("")
   const navigate = useNavigate();
   const { id } = useParams();
 
+  const imgUrl = "https://autoapi.dezinfeksiyatashkent.uz/api/uploads/images/";
 
-  const imgUrl = "https://autoapi.dezinfeksiyatashkent.uz/api/uploads/images/"
-
-  const fetchData = async () => {
-    try {
-      const token = localStorage.getItem("access_token");
-      const response = await fetch(
-        `https://autoapi.dezinfeksiyatashkent.uz/api/categories/${id}`,
-        {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const token = localStorage.getItem("access_token");
+        const response = await fetch(`https://autoapi.dezinfeksiyatashkent.uz/api/categories/${id}`, {
           method: "GET",
           headers: {
             Authorization: `Bearer ${token}`,
           },
+        });
+        if (response.ok) {
+          const data = await response.json();
+          setNameEN(data?.data?.name_en);
+          setNameRU(data?.data?.name_ru);
+          // setImage([imgUrl + data?.data?.image_src]);
+          setPrevImage(imgUrl + data?.data?.image_src)
+        } else {
+          console.log("Failed to fetch data");
         }
-      );
-      if (response.ok) {
-        const data = await response.json();
-        setNameEN(data?.data?.name_en);
-        setNameRU(data?.data?.name_ru);
-        setImage([imgUrl + data?.data?.image_src])
-      } else {
-        console.log("Failed to fetch data");
+      } catch (error) {
+        console.log(error.message);
       }
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
-
-  useEffect(() => {
+    };
     fetchData();
-  }, [id]);
-
-  console.log(images);
+  }, [id, imgUrl]);
 
   const handleSaveClick = async (e) => {
     e.preventDefault();
@@ -52,25 +47,26 @@ const EditCategory = () => {
       const formData = new FormData();
       formData.append("name_en", nameEN);
       formData.append("name_ru", nameRU);
-      images.forEach((image) => {
-        formData.append("images", image);
-      });
-      const response =await fetch(
-        `https://autoapi.dezinfeksiyatashkent.uz/api/categories/${id}`,
-        {
-          method: "PUT",
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-          body: formData,
-        }
-      );
-      if(response.status === 500){
-        toast.error("Malumot saqlashda xatolik yuz berdi!!!")
-      }else{
-        toast.success("Malumot muvaffaqqiyatli o'zgartirildi!",{
-          autoClose:2000
+      if(images.length > 0){
+        images.forEach((image) => {
+          formData.append("images",image)
         })
+      }else{
+        formData.append("image_src",prevImage.replace(imgUrl,""))
+      }
+      const response = await fetch(`https://autoapi.dezinfeksiyatashkent.uz/api/categories/${id}`, {
+        method: "PUT",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        body: formData,
+      });
+      if (response.status === 500) {
+        toast.error("Malumot saqlashda xatolik yuz berdi!!!");
+      } else {
+        toast.success("Malumot muvaffaqqiyatli o'zgartirildi!", {
+          autoClose: 2000,
+        });
         navigate("/");
       }
     } catch (error) {
@@ -78,29 +74,33 @@ const EditCategory = () => {
     }
   };
 
+  const handleImageChange = (e) => {
+    setImage([...e.target.files]);
+  };
+
   return (
     <div className="container">
       <div className="edit-container">
-      <form action="" className="form-container" onSubmit={handleSaveClick}>
-        <img src={images} className="img" alt="" />
-        <input
-          type="text"
-          value={nameEN}
-          onChange={(e) => setNameEN(e.target.value)}
-          className="input-field"
-        />
-        <input
-          type="text"
-          value={nameRU}
-          onChange={(e) => setNameRU(e.target.value)}
-          className="input-field"
-        />
-        <input type="file" className="file-input" onChange={(e) => setImage([...e.target.files])} />
-        <button type="submit" className="save-btn">
-          Save
-        </button>
-      </form>
-    </div>
+        <form action="" className="form-container" onSubmit={handleSaveClick}>
+          <img src={prevImage} className="img" alt="" />
+          <input
+            type="text"
+            value={nameEN}
+            onChange={(e) => setNameEN(e.target.value)}
+            className="input-field"
+          />
+          <input
+            type="text"
+            value={nameRU}
+            onChange={(e) => setNameRU(e.target.value)}
+            className="input-field"
+          />
+          <input type="file" className="file-input" onChange={handleImageChange} />
+          <button type="submit" className="save-btn">
+            Save
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
